a77b3bc6abd4b7dc3e4e56d6081878fa3a90cee2
Mia Pan <miapanyuewei@gmail.com>
2019-10-19 22:08:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   squared=[]
   count=0
   while count<array.length do
-     array(count)
+    squared<< (array[count]**2)
     count=+1
-    squared<<array(count**2)
-  end
+    end
   squared
 end
